2028442d4d45ff19f1dadb704298b09faa5baa61
NilsD <nils.dodenhoff@gmail.com>
2024-09-17 17:33:11 +0200

    init tailwind css

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        <h1 class="text-3xl font-bold underline text-amber-300">
+            Hello world!
+        </h1>
         <h1>{{ message }}</h1>
     </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.{vue,js,ts,jsx,tsx}",
+        "./resources/**/**/*.{vue,js,ts,jsx,tsx}",
+        "./resources/js/Pages/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
